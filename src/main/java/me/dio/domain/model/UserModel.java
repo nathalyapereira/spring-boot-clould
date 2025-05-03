ffdaa1a6da34ea29a96df517e563ae85a3bb0430
nathalyapereira <nathalyperreira741@gmail.com>
2025-05-03 16:57:46 -0300

    feat: Implementando a camada de dominio

---
+++ src/main/java/me/dio/domain/model/UserModel.java
@@ -0,0 +1,83 @@
+package me.dio.domain.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity(name = "tb_user")
+public class UserModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private AccountModel account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private CardModel card;
+
+    @OneToMany(cascade= CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<FeatureModel> feature;
+
+    @OneToMany(cascade= CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<NewsModel> news;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AccountModel getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountModel account) {
+        this.account = account;
+    }
+
+    public CardModel getCard() {
+        return card;
+    }
+
+    public void setCard(CardModel card) {
+        this.card = card;
+    }
+
+    public List<FeatureModel> getFeature() {
+        return feature;
+    }
+
+    public void setFeature(List<FeatureModel> feature) {
+        this.feature = feature;
+    }
+
+    public List<NewsModel> getNews() {
+        return news;
+    }
+
+    public void setNews(List<NewsModel> news) {
+        this.news = news;
+    }
+
+}
